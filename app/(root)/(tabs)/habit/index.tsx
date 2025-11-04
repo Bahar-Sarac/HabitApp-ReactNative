86284bfcf968c75229377f3630a61e909e4f8a7f
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2025-11-04 22:59:20 +0300

    add: tailwind derlenmeye çalışıldı

--- app/(root)/(tabs)/habit/index.tsx
+++ app/(root)/(tabs)/habit/index.tsx
@@ -1,6 +1,5 @@
-import AlmostThere from "@/components/AlmostThere";
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 const HabitScreen = () => {
   return (
@@ -10,9 +9,65 @@ const HabitScreen = () => {
         justifyContent: "center",
         alignItems: "center",
         padding: 20,
+        gap: 20,
       }}
     >
-      <AlmostThere />
+      <View style={{ gap: 20 }} className="w-full flex-col">
+        {/* siyah alan */}
+        <View
+          style={{
+            width: "100%",
+            height: 150,
+            borderRadius: 30,
+            backgroundColor: "#000000ff",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
+        >
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+            }}
+          >
+            <Text style={{ color: "#ffffffff", fontSize: 16 }}>
+              You are almost there!
+            </Text>
+            <Text style={{ color: "#c1c1c1ff" }}>1/3 day goals completed</Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <View
+              style={{
+                width: 80,
+                height: 80,
+                borderRadius: 40,
+                justifyContent: "center",
+                alignItems: "center",
+                borderWidth: 2,
+                borderColor: "#ffffffff",
+              }}
+              className="w-20 h-20 flex-row"
+            >
+              <Text className="text-white text-lg">35%</Text>
+            </View>
+          </View>
+        </View>
+
+        {/* kırmızı alan */}
+        <View className="w-full bg-red-500 flex-row items-center justify-around">
+          <View className="w-20 h-20 rounded-full border border-gray-200"></View>
+          <Text className="text-black text-lg">Bahar Sarac</Text>
+        </View>
+      </View>
     </View>
   );
 };
