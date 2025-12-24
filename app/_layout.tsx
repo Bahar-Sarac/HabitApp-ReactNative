ee61aa5f2dbfbee1cd3724361809721e51bf72a5
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2025-12-24 15:54:48 +0300

    add: main _layout.tsx içine UserProvider eklendi

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,16 @@
+import { UserProvider } from "@/context/UserContext";
 import { Stack } from "expo-router";
+import { View } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(root)" options={{ headerShown: false }} />
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <UserProvider>
+      <View className="flex-1">
+        <Stack>
+          <Stack.Screen name="(root)" options={{ headerShown: false }} />
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
+      </View>
+    </UserProvider>
   );
 }
